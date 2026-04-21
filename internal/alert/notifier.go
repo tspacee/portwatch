3234287d550f5501dd7176252c671de9c540424b
Tyler Space <tspacee@users.noreply.github.com>
2026-04-21 11:24:19 +0800

    feat(alert): add MultiNotifier to fan out alerts to multiple backends
    resolves #6

--- internal/alert/notifier.go
+++ internal/alert/notifier.go
@@ -1,5 +1,7 @@
 package alert
 
+import "sync"
+
 // Notifier is the interface implemented by all alert backends.
 type Notifier interface {
 	// Notify sends the given alert. Implementations should be safe to call
@@ -13,3 +15,31 @@ type NopNotifier struct{}
 
 // Notify implements Notifier. It always returns nil.
 func (NopNotifier) Notify(_ Alert) error { return nil }
+
+// MultiNotifier fans out each alert to a list of Notifiers in order.
+// All notifiers are called even if one returns an error; the first
+// non-nil error encountered is returned to the caller.
+type MultiNotifier struct {
+	mu        sync.RWMutex
+	notifiers []Notifier
+}
+
+// NewMultiNotifier returns a MultiNotifier that dispatches to the given notifiers.
+func NewMultiNotifier(notifiers ...Notifier) *MultiNotifier {
+	return &MultiNotifier{notifiers: notifiers}
+}
+
+// Notify implements Notifier. It calls every contained Notifier and returns
+// the first error encountered, continuing to notify remaining backends regardless.
+func (m *MultiNotifier) Notify(a Alert) error {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	var firstErr error
+	for _, n := range m.notifiers {
+		if err := n.Notify(a); err != nil && firstErr == nil {
+			firstErr = err
+		}
+	}
+	return firstErr
+}
